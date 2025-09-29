9c2d431776f594b76c15e46ca19506f13f2f9fbf
BarbaraArao <barbara.paula.arao@outlook.com>
2025-09-29 05:33:01 +0100

    enabling cors filter localhost...

--- src/main/java/CorsFilter.java
+++ src/main/java/CorsFilter.java
@@ -9,7 +9,7 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:4200");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
