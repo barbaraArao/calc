7c9458c8aacd8bde1eb7ffc38a32e92e24fe467a
BarbaraArao <barbara.paula.arao@outlook.com>
2025-09-29 07:16:40 +0100

    removing redis

--- src/main/java/org/altice/services/CalcService.java
+++ src/main/java/org/altice/services/CalcService.java
@@ -1,8 +1,6 @@
 package org.altice.services;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import redis.RedisCacheService;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -13,9 +11,6 @@ public class CalcService implements ICalcService {
 
     private final List<BigInteger> localCache = new ArrayList<>();
     private static final int CACHE_THRESHOLD = 100_000;
-
-    @Inject
-    RedisCacheService redisCache;
 
     public CalcService() {
         localCache.add(BigInteger.ZERO);
@@ -30,24 +25,11 @@ public class CalcService implements ICalcService {
             return localCache.get(n);
         }
 
-        if (n >= CACHE_THRESHOLD) {
-            String cached = redisCache.get("calc:" + n);
-            if (cached != null) {
-                return new BigInteger(cached);
-            }
-        }
-
         for (int i = localCache.size(); i <= n; i++) {
             BigInteger next = localCache.get(i - 4).add(localCache.get(i - 3));
             localCache.add(next);
         }
 
-        BigInteger result = localCache.get(n);
-
-        if (n >= CACHE_THRESHOLD) {
-            redisCache.set("calc:" + n, result.toString());
-        }
-
-        return result;
+        return localCache.get(n);
     }
 }
